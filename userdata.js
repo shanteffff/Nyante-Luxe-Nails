287c7837e8f304f45c823ad86eae024b61a4a0b3
Shante Smith <shantesmith460@gmail.com>
2026-07-22 13:24:43 -0500

    Refactor showUserFrequency function for clarity

--- userdata.js
+++ userdata.js
@@ -49,60 +49,18 @@ function showUserFrequency() {
     // Display Gender Results
     document.getElementById("genderResults").innerHTML = `
         <h3>Gender Frequency</h3>
-
         <p>Male: ${male}</p>
         <p>Female: ${female}</p>
         <p>Other: ${other}</p>
     `;
 
-    // Display Age Results
+    // Display Age Group Results
     document.getElementById("ageResults").innerHTML = `
         <h3>Age Group Frequency</h3>
-
-        <p>18 - 25 : ${age18to25}</p>
-        <p>26 - 35 : ${age26to35}</p>
-        <p>36 - 50 : ${age36to50}</p>
-        <p>50+ : ${age50plus}</p>
+        <p>18 - 25: ${age18to25}</p>
+        <p>26 - 35: ${age26to35}</p>
+        <p>36 - 50: ${age36to50}</p>
+        <p>50+: ${age50plus}</p>
     `;
-
-}
-        if (user.gender === "Male") {
-            male++;
-        }
-        else if (user.gender === "Female") {
-            female++;
-        }
-        else {
-            other++;
-        }
-
-        // Age Group Count
-        const age = Number(user.age);
-
-        if (age >= 18 && age <= 25) {
-            age18to25++;
-        }
-        else if (age >= 26 && age <= 35) {
-            age26to35++;
-        }
-        else if (age >= 36 && age <= 50) {
-            age36to50++;
-        }
-        else if (age > 50) {
-            age50plus++;
-        }
-
-    });
-
-    console.log("Gender Frequency");
-    console.log("Male:", male);
-    console.log("Female:", female);
-    console.log("Other:", other);
-
-    console.log("Age Groups");
-    console.log("18-25:", age18to25);
-    console.log("26-35:", age26to35);
-    console.log("36-50:", age36to50);
-    console.log("50+:", age50plus);
 
 }
